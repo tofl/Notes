0fb0d3c75704bac5eedcb2751f7d5fa8e68f9099
Tom F <thomas.flitterman@ynov.com>
2019-06-09 21:11:44 +0200

    Created initial component

---
+++ src/containers/Notes/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+// Custom imports
+
+const Notes = props => {
+	return (
+		<div class="main">
+		
+		</div>
+	);
+};
+
+export default Notes;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// Custom imports
+import Notes from './containers/Notes';
+
+ReactDOM.render(
+	<Notes />,
+	document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
