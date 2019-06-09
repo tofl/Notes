9cc9d702f9dba99c09d6ddc5c624e385cf7a6572
Tom F <thomas.flitterman@ynov.com>
2019-06-10 01:15:21 +0200

    Retrieve the notes

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './style.css';
+
+const Sidebar = props => {
+	return (
+		<div className="sidebar">
+			<ul>
+				<li>New note...</li>
+			</ul>
+		</div>
+	);
+};
+
+export default Sidebar;
--- src/containers/Notes/index.js
+++ src/containers/Notes/index.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './style.css';
 
 // Custom imports
+import Sidebar from './../../components/Sidebar';
+import getCookieByName from './../../services/getCookieByName';
 
-const Notes = props => {
-	return (
-		<div class="main">
+class Notes extends Component {
+	
+	state = {
+		notes: null
+	};
+	
+	componentDidMount() {
 		
-		</div>
-	);
+		if (getCookieByName("notes") !== '') {
+			this.setState({ notes: JSON.parse(getCookieByName("notes"))});
+		}
+	}
+	
+	render() {
+		return (
+			<div className="main">
+				<Sidebar />
+			</div>
+		);
+	}
 };
 
 export default Notes;
