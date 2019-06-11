7f26cc9e6eb60f373b4ff1d1bc25bd6bf12a9ddc
Tom F <thomas.flitterman@ynov.com>
2019-06-11 14:38:12 +0200

    Modifications

--- src/components/SelectedNote/index.js
+++ src/components/SelectedNote/index.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
-const SelectedNote = () => (
-	<div>A note was selected</div>
-);
+// Custom imports
+import './../../services/getCookieByName';
+import getCookieByName from "../../services/getCookieByName";
+
+const SelectedNote = props => {
+	let cookie = JSON.parse(getCookieByName("notes"));
+	
+	return (
+		<p>{ cookie[props.match.params.id].name }</p>
+	)
+};
 
 export default SelectedNote;
